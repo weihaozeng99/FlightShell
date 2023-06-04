1dd1962530acd906acb89b844cfa83a9527e95b3
weihaozeng99 <weihao_zeng@brown.edu>
2023-06-04 16:49:04 -0400

    url get is denied by website

--- FightSearch.py
+++ FightSearch.py
@@ -1,1 +1,18 @@
 from bs4 import BeautifulSoup
+from urllib import request
+
+link_head="https://www.kayak.com/flights/"
+from2des="BOS,NYC-TYO"
+date="/2023-07-03"
+link_end="?sort=bestflight_a"
+
+
+page_addr=link_head+from2des+date+link_end
+def check_price(page_addr):
+    page=request.urlopen(page_addr)
+    soup=BeautifulSoup(page,'html.parser')
+    test=soup.find_all(class_="nrc6")
+    print(test)
+
+# def has_result():
+check_price(page_addr)
