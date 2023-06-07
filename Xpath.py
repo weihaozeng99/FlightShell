1caf9df0d0f67d5730688fffcd472c74d7f744d7
weihaozeng99 <weihao_zeng@brown.edu>
2023-06-06 23:18:15 -0400

    update

--- Xpath.py
+++ Xpath.py
@@ -1,9 +1,94 @@
 import time
 from selenium import webdriver
-link_head="https://www.kayak.com/flights/"
-from2des="BOS,NYC-TYO"
-date="/2023-07-03"
-link_end="?sort=bestflight_a"
-page_addr=link_head+from2des+date+link_end
-driver=webdriver.Chrome()
-driver.get(page_addr)
+from selenium.webdriver.common.by import By
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+#info: [0"carrier",1"price",2"duration",3"stops",4"date"]
+def send_mail(info):
+    content="<strong>Date: </strong>"+info[4]+"/n<strong>Carrier Name: </strong>"+info[0]+"/n<strong>Price: </strong>"+info[1]+"/n<strong>Duration: </strong>"+info[2]+"/n<strong>Stops: </strong>"+info[3]
+    message = Mail(
+    from_email='zengmei888@gmail.com',
+    to_emails='zengweihao99@gmail.com',
+    subject="Ticket Info Update",
+    #html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    html_content=content)
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+
+def scrap(date_desire):
+    date="2023-07-03"
+    link_head="https://www.kayak.com/flights/"
+    from2des="BOS,NYC-TYO/"
+    date=date_desire
+    link_end="?sort=bestflight_a"
+
+    page_addr=link_head+from2des+date+link_end
+    driver=webdriver.Chrome()
+    driver.get(page_addr)
+    time.sleep(3)
+
+    flights_xpath='//div[@class="nrc6"]'
+    flights=driver.find_elements(By.XPATH, flights_xpath)
+    #print(flights[0])
+    #test_id=flights[0].get_attribute("")
+    # print(test)
+    for i in range(0,5):
+        try:
+            more_button_path='//div[contains(@class,"show-more-button")]'
+            driver.find_element(By.XPATH,more_button_path).click()
+            time.sleep(15)
+        except:
+            pass   
+    carrier_name_path='//div[@class="c_cgF c_cgF-mod-variant-default" and @dir="auto"]'
+    duration_path='//div[@class="xdW8"]/div[@class="vmXl vmXl-mod-variant-default"]'
+    stops_path='//span[contains(@class,"stops-text")]'
+    price_path='//div[@class="f8F1-price-text"]'
+    carriers=driver.find_elements(By.XPATH,carrier_name_path)
+    durations=driver.find_elements(By.XPATH,duration_path)
+    stops=driver.find_elements(By.XPATH,stops_path)
+    prices=driver.find_elements(By.XPATH,price_path)
+    print(len(carriers))
+    print(len(stops))
+    print(len(prices))
+    # if len(carriers)!=len(stops) or len(stops)!=len(prices):
+
+    #     return
+    price_number=[]
+    stop_number=[]
+    for i in range(0,len(stops)):
+        price_temp=prices[i].text
+        price_temp=price_temp.replace('$',"")
+        price_number.append(int(price_temp.replace(',',"")))
+        stop_temp=stops[i].text
+        if stop_temp=="nonstop":
+            stop_temp=0
+        else:
+            stop_temp=int(stop_temp[0])
+        stop_number.append(stop_temp)
+    # print(price_number)
+
+    sorted_price_id = sorted(range(len(price_number)), key=lambda k: price_number[k], reverse=False)
+    sorted_stop_id = sorted(range(len(stop_number)), key=lambda k: stop_number[k], reverse=False)
+    # for i in range(0,len(stops)):
+    #     print(stops[i].text)
+    # test_path=test_path+test_id
+    # test_path=test_path+'/div[@class="c_cgF c_cgF-mod-variant-default"]'
+    info_to_send=[]
+    for i in range(0,len(sorted_price_id)):
+        if price_number[sorted_price_id[i]]<=900:
+            temp=[carriers[sorted_price_id[i]],str(price_number[sorted_price_id[i]]),durations[sorted_price_id[i]],str(stop_number[sorted_price_id[i]]),date]
+            info_to_send.append(temp)
+    for info_single in info_to_send:
+        send_mail(info_single)
+date_year_month="2023-06-"
+date_day=14
+while(1):
+    for i in range(0,15):
+        scrap(date_year_month+str(date_day+i))
